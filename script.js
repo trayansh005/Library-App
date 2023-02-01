1a4b81f940d703fdaaba376479a247c487905009
trayansh <74540362+trayansh005@users.noreply.github.com>
2023-02-01 10:43:13 +0530

    added buttons to read and remove

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const pages = document.querySelector("#pages");
 const read = document.querySelector("#read");
 const shelf = document.querySelector(".shelf");
 
-const bookContainer = document.querySelector("#book");
+const bookContainer = document.querySelector(".book");
 const modalBox = document.querySelector(".book-details-popup");
 
 let myLibrary = [];
@@ -45,7 +45,7 @@ submitBookBtn.addEventListener("click", () => {
 	console.log(myLibrary);
 
 	const newDiv = document.createElement("div");
-	newDiv.id = "book";
+	newDiv.classList.add("book");
 	const newDiv2 = document.createElement("div");
 	newDiv2.classList.add("book-info");
 	newDiv2.innerHTML = `<p id="book-title">${title.value}</p>
@@ -55,15 +55,54 @@ submitBookBtn.addEventListener("click", () => {
 	const bookImg = document.createElement("img");
 	bookImg.src = "images/book-education-closed-free-vector-removebg-preview.png";
 	newDiv.appendChild(bookImg);
-	const checkMark = document.createElement("img");
-	checkMark.src = "images/checkmark.png";
-	checkMark.id = "read-icon";
-	newDiv.appendChild(checkMark);
+	// const checkMark = document.createElement("img");
+	// checkMark.src = "images/checkmark.png";
+	// checkMark.id = "read-icon";
+	// newDiv.appendChild(checkMark);
 	const span = document.createElement("span");
 	span.id = "book-pages";
 	span.innerHTML = `${pages.value}`;
 	newDiv.appendChild(span);
+
+	const newDiv3 = document.createElement("div");
+	newDiv3.classList.add("buttons");
+	const markButton = document.createElement("button");
+	const removeButton = document.createElement("button");
+	markButton.classList.add("mark-read");
+	removeButton.classList.add("remove");
+	markButton.innerHTML = "Read";
+	removeButton.innerHTML = "Remove";
+
+	newDiv3.appendChild(markButton);
+	newDiv3.appendChild(removeButton);
+	newDiv.appendChild(newDiv3);
+
 	shelf.insertBefore(newDiv, addBookPlaceholder);
 
 	modalBox.style.visibility = "hidden";
+
+	addButtons();
 });
+
+function addButtons() {
+	let bookDivs = document.querySelectorAll(".book");
+	for (let i = 0; i < bookDivs.length; i++) {
+		let markRead = bookDivs[i].querySelectorAll(".mark-read");
+		let removeBtn = bookDivs[i].querySelectorAll(".remove");
+		for (let j = 0; j < markRead.length; j++) {
+			markRead[j].addEventListener("click", () => {
+				console.log("clicked");
+				const checkMark = document.createElement("img");
+				checkMark.src = "images/checkmark.png";
+				checkMark.id = "read-icon";
+				bookDivs[i].appendChild(checkMark);
+			});
+		}
+
+		for (let y = 0; y < removeBtn.length; y++) {
+			removeBtn[y].addEventListener("click", () => {
+				bookDivs[i].remove();
+			});
+		}
+	}
+}
